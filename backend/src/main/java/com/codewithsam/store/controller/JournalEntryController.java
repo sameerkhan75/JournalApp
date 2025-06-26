5f2b0518c811091318933f3c2ef9091b13d0509d
samee <sameer754811@gmail.com>
2025-06-26 16:32:48 +0530

    message

--- backend/src/main/java/com/codewithsam/store/controller/JournalEntryController.java
+++ backend/src/main/java/com/codewithsam/store/controller/JournalEntryController.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/journal")
 public class JournalEntryController {
 
